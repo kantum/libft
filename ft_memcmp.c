0387f0df4ddc63cf3f030363b452e1115b280bf4
Quentin DUROT <qdurot@.42.fr>
2017-04-15 21:34:09 +0200

    ft_memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -3,12 +3,18 @@
 int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t			i;
-	unsigned char	s1_tmp;
-	unsigned char	s2_tmp;
+	unsigned char	*s1_tmp;
+	unsigned char	*s2_tmp;
 
+	i = 0;
+	s1_tmp = (unsigned char *)s1;
+	s2_tmp = (unsigned char *)s2;
 	while (i < n)
 	{
-		i++;
+		if (s1_tmp[i] == s2_tmp[i])
+			i++;
+		else
+			return (s1_tmp[i] - s2_tmp[i]);
 	}
 	return (0);
 }
