9cea60f0775067d41bfc3e60cba9c8ca4f8773b8
Quentin DUROT <qdurot@.42.fr>
2017-04-20 19:54:05 +0200

    strsub strtrim

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,12 +6,30 @@
 /*   By: qdurot <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/18 20:14:16 by qdurot            #+#    #+#             */
-/*   Updated: 2017/04/19 17:18:42 by qdurot           ###   ########.fr       */
+/*   Updated: 2017/04/20 17:11:01 by qdurot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strsub(char const *s1, unsigned int start, size_t len);
+char	*ft_strsub(char const *s1, unsigned int start, size_t len)
 {
+	char			*ret;
+	unsigned int	i;
+	unsigned int	k;
+
+	i = 0;
+	k = 0;
+	while (i < start)
+		i++;
+	if (!(ret = (char *)malloc(sizeof(char) * len)))
+		return (NULL);
+	while (i < (start + len))
+	{
+		ret[k] = s1[i];
+		k++;
+		i++;
+	}
+	ret[k] = '\0';
+	return (ret);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: qdurot <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/18 20:52:16 by qdurot            #+#    #+#             */
-/*   Updated: 2017/04/18 20:52:46 by qdurot           ###   ########.fr       */
+/*   Updated: 2017/04/20 19:39:45 by qdurot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,38 @@
 
 char	*ft_strtrim(char const *s)
 {
+	char	*ret;
+	size_t	i;
+	size_t	k;
+	size_t	len;
+
+	i = 0;
+	k = 0;
+	len = 0;
+	while (s[len + 1])
+		len++;
+	while (s[i] == ' ' || s[i] == '\n' || s[i] == '\t')
+		i++;
+	while (len && (s[len] == ' ' || s[len] == '\n' || s[len] == '\t'))
+		len--;
+	if (i > len)
+	{
+		if (!(ret = (char *)malloc(sizeof(char))))
+			return (ret);
+		ret = "";
+		return (ret);
+	}
+	else
+	{
+		if (!(ret = (char *)malloc(sizeof(char) * (len - i + 1))))
+			return (NULL);
+	}
+	while (i <= len)
+	{
+		ret[k] = s[i];
+		i++;
+		k++;
+	}
+	ret[k] = '\0';
+	return (ret);
 }
