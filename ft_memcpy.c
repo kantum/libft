a2238b04ba1c51d8be8ce82f75351248b3684ee0
Quentin DUROT <qdurot@.42.fr>
2017-05-02 21:01:01 +0200

    1opt

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: qdurot <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/16 15:19:28 by qdurot            #+#    #+#             */
-/*   Updated: 2017/04/16 15:19:31 by qdurot           ###   ########.fr       */
+/*   Updated: 2017/05/02 20:56:13 by qdurot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,18 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	while (n--)
+	while (n-- % 8)
 		((unsigned char *)dst)[n] = ((unsigned char *)src)[n];
+	while (n-- % 8)
+	{
+		((unsigned char *)dst)[n] = ((unsigned char *)src)[n];
+		((unsigned char *)dst)[n] = ((unsigned char *)src)[n];
+		((unsigned char *)dst)[n] = ((unsigned char *)src)[n];
+		((unsigned char *)dst)[n] = ((unsigned char *)src)[n];
+		((unsigned char *)dst)[n] = ((unsigned char *)src)[n];
+		((unsigned char *)dst)[n] = ((unsigned char *)src)[n];
+		((unsigned char *)dst)[n] = ((unsigned char *)src)[n];
+		((unsigned char *)dst)[n] = ((unsigned char *)src)[n];
+	}
 	return (dst);
 }
--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: qdurot <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/16 15:19:34 by qdurot            #+#    #+#             */
-/*   Updated: 2017/04/16 15:19:37 by qdurot           ###   ########.fr       */
+/*   Updated: 2017/05/02 20:57:29 by qdurot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,20 @@
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	while (len--)
-		((unsigned char *)b)[len] = (unsigned char)c;
+	while (len % 8)
+	{
+		((unsigned char *)b)[--len] = (unsigned char)c;
+	}
+	while (len)
+	{
+		((unsigned char *)b)[--len] = (unsigned char)c;
+		((unsigned char *)b)[--len] = (unsigned char)c;
+		((unsigned char *)b)[--len] = (unsigned char)c;
+		((unsigned char *)b)[--len] = (unsigned char)c;
+		((unsigned char *)b)[--len] = (unsigned char)c;
+		((unsigned char *)b)[--len] = (unsigned char)c;
+		((unsigned char *)b)[--len] = (unsigned char)c;
+		((unsigned char *)b)[--len] = (unsigned char)c;
+	}
 	return (b);
 }
