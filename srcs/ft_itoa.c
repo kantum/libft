5a1bec4e4bf05492e2f131458a7074abba9a2b05
Vimman <vimman@users.noreply.github.com>
2017-12-17 20:26:11 +0100

    leak in ft_itoa

--- srcs/ft_itoa.c
+++ srcs/ft_itoa.c
@@ -15,12 +15,10 @@
 char		*ft_itoa(int n)
 {
 	size_t	i;
-	char	*ret;
+	static char	ret[12];
 	int		sign;
 
 	i = 0;
-	if (!(ret = (char *)malloc(sizeof(char) * 12)))
-		return (0);
 	if (n == 0)
 		return (ft_strdup("0"));
 	sign = (n < 0) ? 1 : 0;
